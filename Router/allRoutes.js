2e2c35e557ef8687d0f43c3aa56d547270db104a
eurajane <jbouchend@gmail.com>
2024-12-06 21:44:31 +0100

    update responsive navbar

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/createCalendar", "Créer un calendrier", "/pages/createCalendar.html"),
+    new Route("/example", "Exemple", "/pages/example.html"),
+    new Route("/register", "Inscription", "/pages/register.html"),
+    new Route("/login", "Connexion", "/pages/login.html", "/pages/login.js"),
+    new Route("/login2", "Connexion2", "/pages/login2.html", "/pages/login.js"),
+    new Route("/legacy", "Mentions légales", "/pages/legacy.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Osez Noël";
