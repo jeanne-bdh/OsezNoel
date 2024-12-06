22f53748622db5f5c79ff2c74db6d79db7f02cc5
eurajane <jbouchend@gmail.com>
2024-12-06 10:08:11 +0100

    ajout des pages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,13 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("createCalendar", "Create Calendar", "/pages/createCalendar.html"),
+    new Route("example", "Example", "/pages/example.html"),
+    new Route("register", "Register", "/pages/register.html"),
+    new Route("login", "Login", "/pages/login.html"),
+    new Route("legacy", "Mentions légales", "/pages/legacy.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Osez Noël";
