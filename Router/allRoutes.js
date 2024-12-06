9d7fcb0f72b63bdacc0738457251be92f1a371db
eurajane <jbouchend@gmail.com>
2024-12-06 14:42:05 +0100

    test ok

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,11 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("createCalendar", "Create Calendar", "/pages/createCalendar.html"),
-    new Route("example", "Example", "/pages/example.html"),
-    new Route("register", "Register", "/pages/register.html"),
-    new Route("login", "Login", "/pages/login.html"),
-    new Route("legacy", "Mentions légales", "/pages/legacy.html"),
+    new Route("/createCalendar", "Créer un calendrier", "/pages/createCalendar.html"),
+    new Route("/example", "Example", "/pages/example.html"),
+    new Route("/register", "Inscription", "/pages/register.html"),
+    new Route("/login", "Connexion", "/pages/login.html"),
+    new Route("/legacy", "Mentions légales", "/pages/legacy.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
