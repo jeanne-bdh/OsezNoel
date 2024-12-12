3991417d109866aaeac64209481e08b90da581ea
eurajane <jbouchend@gmail.com>
2024-12-12 18:09:51 +0100

    ajout page example2 pour test

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/createCalendar", "Créer un calendrier", "/pages/createCalendar.html"),
-    new Route("/example", "Exemple", "/pages/example.html"),
+    new Route("/example", "Exemple", "/pages/example2.html", "/pages/example2.js"),
     new Route("/register", "Inscription", "/pages/register.html"),
     new Route("/login", "Connexion", "/pages/login.html", "/pages/login.js"),
     new Route("/legacy", "Mentions légales", "/pages/legacy.html"),
