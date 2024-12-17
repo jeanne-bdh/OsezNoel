5753def45e40d9daf958528e85e784e03d9f4692
eurajane <jbouchend@gmail.com>
2024-12-17 10:11:55 +0100

    ajout hover sur bouton

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,10 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/login", "Connexion", "/pages/login.html", "/pages/login.js"),
+    new Route("/register", "Inscription", "/pages/register.html", "/pages/register.js"),
+
     new Route("/createCalendar", "Créer un calendrier", "/pages/createCalendar.html"),
     new Route("/example", "Exemple", "/pages/example2.html", "/pages/example2.js"),
-    new Route("/register", "Inscription", "/pages/register.html", "/pages/register.js"),
-    new Route("/login", "Connexion", "/pages/login.html", "/pages/login.js"),
     new Route("/legacy", "Mentions légales", "/pages/legacy.html"),
 ];
 
