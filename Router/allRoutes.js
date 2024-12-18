9427a3c5e59019e2b7bf0e1b50ba0a5f50059e82
eurajane <jbouchend@gmail.com>
2024-12-18 17:01:19 +0100

    ajout pages mentions légales

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,10 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/login", "Connexion", "/pages/login.html", "/pages/login.js"),
     new Route("/register", "Inscription", "/pages/register.html", "/pages/register.js"),
-
-    new Route("/createCalendar", "Créer un calendrier", "/pages/createCalendar.html"),
-    new Route("/example", "Exemple", "/pages/example.html", "/pages/example.js"),
-    new Route("/legacy", "Mentions légales", "/pages/legacy.html"),
+    new Route("/createCalendar", "Formulaire de création calendrier", "/pages/formCalendar.html"),
+    new Route("/example", "Exemple de calendrier", "/pages/example.html", "/pages/example.js"),
+    new Route("/legalinfos", "Mentions légales", "/pages/legalinfos.html"),
+    new Route("/privacyPolicy", "Politique de confidentialité", "/pages/privacyPolicy.html"),
+    new Route("/CGU", "CGU", "/pages/CGU.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
