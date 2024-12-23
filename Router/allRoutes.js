38bb1ac1824e7b8ab4e115db987b6925cb7382e3
eurajane <jbouchend@gmail.com>
2024-12-23 18:17:59 +0100

    update

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,13 +3,6 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-<<<<<<< HEAD
-    new Route("/createCalendar", "Créer un calendrier", "/pages/createCalendar.html"),
-    new Route("/example", "Exemple", "/pages/example2.html", "/pages/example2.js"),
-    new Route("/register", "Inscription", "/pages/register.html", "/pages/register.js"),
-    new Route("/login", "Connexion", "/pages/login.html", "/pages/login.js"),
-    new Route("/legacy", "Mentions légales", "/pages/legacy.html"),
-=======
     new Route("/login", "Connexion", "/pages/login.php", "/pages/login.js"),
     new Route("/register", "Inscription", "/pages/register.html", "/pages/register.js"),
     new Route("/createCalendar", "Formulaire de création calendrier", "/pages/formCalendar.html"),
@@ -17,7 +10,6 @@ export const allRoutes = [
     new Route("/legalinfos", "Mentions légales", "/pages/legalinfos.html"),
     new Route("/privacyPolicy", "Politique de confidentialité", "/pages/privacyPolicy.html"),
     new Route("/CGU", "CGU", "/pages/CGU.html"),
->>>>>>> develop
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
