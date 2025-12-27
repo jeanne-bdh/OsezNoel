c68110536c5cb9a4cf80fa7a90c176759101ee25
eurajane <jbouchend@gmail.com>
2025-12-27 17:56:02 +0100

    install stacks

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <footer className="text-gray-600 body-font">
+            <div className="border-t border-gray-200">
+                
+            </div>
+            <div className="bg-gray-100">
+                <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                    <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
+                        <a href="/" className="text-gray-600 ml-1" target="_blank" rel="noopener noreferrer">@knyttneve</a>
+                    </p>
+                    <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Enamel pin tousled raclette tacos irony</span>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
